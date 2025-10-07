754aa31697f26b9d0b790540b4640e3d21781717
Your Name <you@example.com>
2025-10-07 13:45:47 +0300

    Add a little more testing

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,21 @@ mod evaluator;
 
 fn main() {
     let mut eval = TreeEvaluator::new();
-    // 1 + 3 / 2 / 2 + 2
+    // 1.67 + 3.66 / 2 / 3.66 * 2 + 2 - 3 = 1.67
     let tokens = [
-        ExpressionToken::Number(1.0),
+        ExpressionToken::Number(1.67),
         ExpressionToken::Operation(evaluator::Operation::Add),
+        ExpressionToken::Number(3.66),
+        ExpressionToken::Operation(evaluator::Operation::Divide),
+        ExpressionToken::Number(2.0),
+        ExpressionToken::Operation(evaluator::Operation::Divide),
+        ExpressionToken::Number(3.66),
+        ExpressionToken::Operation(evaluator::Operation::Multiply),
+        ExpressionToken::Number(2.0),
+        ExpressionToken::Operation(evaluator::Operation::Add),
+        ExpressionToken::Number(2.0),
+        ExpressionToken::Operation(evaluator::Operation::Subtract),
         ExpressionToken::Number(3.0),
-        ExpressionToken::Operation(evaluator::Operation::Divide),
-        ExpressionToken::Number(2.0),
-        ExpressionToken::Operation(evaluator::Operation::Divide),
-        ExpressionToken::Number(2.0),
-        ExpressionToken::Operation(evaluator::Operation::Add),
-        ExpressionToken::Number(2.0),
     ];
     
     eval.load_tokens(&tokens);
