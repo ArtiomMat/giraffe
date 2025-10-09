84cfe6eb3866a795661e33db5e3b7a1b8375a84a
Your Name <you@example.com>
2025-10-10 00:53:29 +0300

    Make main reflect the new enum

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,63 @@
-use evaluator::{ExpressionEvaluator, ExpressionToken, TreeEvaluator};
+use evaluator::{ExpressionEvaluator, TreeEvaluator};
+use tokenizer::ExpressionToken;
 
 mod evaluator;
+mod tokenizer;
 
 fn main() {
     let mut eval = TreeEvaluator::new();
-    
+
     // 1.67 + 3.66 / 2 / 3.66 * 2 + 2 - 3
     // = 1.67
     let tokens = [
         ExpressionToken::Number(1.67),
-        ExpressionToken::Operation(evaluator::Operation::Add),
+        ExpressionToken::Plus,
         ExpressionToken::Number(3.66),
-        ExpressionToken::Operation(evaluator::Operation::Divide),
+        ExpressionToken::Slash,
         ExpressionToken::Number(2.0),
-        ExpressionToken::Operation(evaluator::Operation::Divide),
+        ExpressionToken::Slash,
         ExpressionToken::Number(3.66),
-        ExpressionToken::Operation(evaluator::Operation::Multiply),
+        ExpressionToken::Star,
         ExpressionToken::Number(2.0),
-        ExpressionToken::Operation(evaluator::Operation::Add),
+        ExpressionToken::Plus,
         ExpressionToken::Number(2.0),
-        ExpressionToken::Operation(evaluator::Operation::Subtract),
+        ExpressionToken::Minus,
         ExpressionToken::Number(3.0),
     ];
-    
+
     eval.load_tokens(&tokens);
-    println!("{}", eval.evaluate(&[]));
+    println!(
+        "\n1.67 + 3.66 / 2 / 3.66 * 2 + 2 - 3\n{}\n",
+        eval.evaluate(&[])
+    );
 
     // 3 * (1.67 + 3.66) / 2 / 3.66 * 2 + 2 - 3
     // = 5.7377043
     let tokens = [
         ExpressionToken::Number(3.0),
-        ExpressionToken::Operation(evaluator::Operation::Multiply),
+        ExpressionToken::Star,
         ExpressionToken::OpenBracket,
         ExpressionToken::Number(1.67),
-        ExpressionToken::Operation(evaluator::Operation::Add),
+        ExpressionToken::Plus,
         ExpressionToken::Number(3.66),
         ExpressionToken::CloseBracket,
-        ExpressionToken::Operation(evaluator::Operation::Divide),
+        ExpressionToken::Slash,
         ExpressionToken::Number(2.0),
-        ExpressionToken::Operation(evaluator::Operation::Divide),
+        ExpressionToken::Slash,
         ExpressionToken::Number(3.66),
-        ExpressionToken::Operation(evaluator::Operation::Multiply),
+        ExpressionToken::Star,
         ExpressionToken::OpenBracket,
         ExpressionToken::Number(2.0),
-        ExpressionToken::Operation(evaluator::Operation::Add),
+        ExpressionToken::Plus,
         ExpressionToken::Number(2.0),
         ExpressionToken::CloseBracket,
-        ExpressionToken::Operation(evaluator::Operation::Subtract),
+        ExpressionToken::Minus,
         ExpressionToken::Number(3.0),
     ];
-    
+
     eval.load_tokens(&tokens);
-    println!("{}", eval.evaluate(&[]));
+    println!(
+        "3 * (1.67 + 3.66) / 2 / 3.66 * 2 + 2 - 3\n{}\n",
+        eval.evaluate(&[])
+    );
 }
